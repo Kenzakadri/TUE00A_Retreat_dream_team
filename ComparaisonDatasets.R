002a4d7dd7440c643e3d6fdd6aa87d2d513fcdbf
Kenza <50607212+Kenzakadri@users.noreply.github.com>
2024-04-18 11:05:52 +0200

    push

---
+++ ComparaisonDatasets.R
@@ -0,0 +1,22 @@
+# rm(list=ls()) #Clear the work space
+dev.off() # clear all plots
+
+#### Settings and loading the data ####
+
+dataFolder="~/Documents/GitHub/TUE00A_Retreat_dream_team/datasets""
+dev.off() #Clear all plots
+#Load packages
+library(data.table)
+library(ggplot2)
+library(magrittr)
+library(xml2)
+library(ggpubr)
+library(tidyverse)
+library(ggcorrplot)
+library(corrplot)
+library(glmnet)
+library(sjPlot)
+library(ggpubr) 
+library(nloptr)
+library(dplyr)
+
