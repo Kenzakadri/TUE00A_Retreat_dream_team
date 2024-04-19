cee0ed676487a4499212dabbe25a046856cd8abe
lenarddome <lenarddome@gmail.com>
2024-04-19 10:43:51 +0200

    transitional commit

--- ComparaisonDatasets-all-items.R
+++ ComparaisonDatasets-all-items.R
@@ -44,15 +44,21 @@ oci_plot <- rbind(data.table(source = "gillan", value = OCI_G),
 
 ggplot(oci_plot, aes(x = value, fill = source)) + geom_density(alpha = 0.5)
 
+ggsave('datasets/oci_plot.png')
+
 lsas_plot <- rbind(data.table(source = "gillan", value = LSAS_G),
       data.table(source = "scholl", value = LSAS_S))
 
 ggplot(lsas_plot, aes(x = value, fill = source)) + geom_density(alpha = 0.5)
 
+ggsave('datasets/lsas_plot.png')
+
 schizo <- rbind(data.table(source = "gillan", value = SCZ_G),
       data.table(source = "scholl", value = SCZ_S))
 
 ggplot(schizo, aes(x = value, fill = source)) + geom_density(alpha = 0.5)
+
+ggsave('datasets/schizo_plot.png')
 
 SCZ_G <- Gillan %>% select(SCZ_24:SCZ_33)
 SCZ_G <- cbind(SCZ_G, Gillan %>% select(SCZ_1:SCZ_12))
@@ -67,8 +73,12 @@ LSAS_S <- Scholl %>% select(SocialAnx.Q1:SocialAnx.Q24)
 
 
 
-gillan_fa <- fa.parallel(cbind(OCI_G, SCZ_G, LSAS_G))
-scholl_fa <- fa.parallel(na.omit(cbind(OCI_S, SCZ_S, LSAS_S)))
+gillan_fa <- fa.parallel(mixedCor(cbind(SCZ_G, OCI_G, LSAS_G), method='spearman', d=1:22, c=23:64)$rho, n.iter = 20, n.obs = nrow(Gillan))
+
+scholl_fa <- fa.parallel(mixedCor(na.omit(cbind(SCZ_S, OCI_S, LSAS_S)), method='spearman', d=1:22, c=23:64)$rho)
+
+
+########################################################################
 
 plot(gillan_fa)
 plot(scholl_fa)
@@ -83,32 +93,65 @@ STA_G <- Gillan %>% select(STAI_1:STAI_20)
 STA_S <- Scholl %>% select(traitAnx.Q1:traitAnx.Q20)
 
 
-mixedCor(cbind(SCZ_G, OCI_G, LSAS_G, AES_G, SDS_G, STA_G), method = 'spearman', d = 1:22)
 
 gillan_fa_full <- fa.parallel(
     mixedCor(cbind(SCZ_G, OCI_G, LSAS_G, AES_G, SDS_G, STA_G), method = 'spearman',
-             d = 1:22, p = 23:122)$rho,
+             d = 1:22, c = 23:122)$rho,
     n.iter = 20, n.obs = nrow(Gillan)
     )
 
 scholl_fa_full <- fa.parallel(
     mixedCor(na.omit(cbind(SCZ_S, OCI_S, LSAS_S, AES_S, SDS_S, STA_S)),
-             method = 'spearman', d = 1:22, p = 23:123)$rho,
+             method = 'spearman', d = 1:22, c = 23:123)$rho,
     n.iter = 20, n.obs = nrow(Scholl)
     )
 
-fa.diagram(
-    mixedCor(cbind(SCZ_G, OCI_G, LSAS_G, AES_G, SDS_G, STA_G),
-             method = 'spearman', d = 1:22, d = 1:22, p = 23:123)$rho
-             )
+gillan_plotting_fa <- cbind(SCZ_G, OCI_G, LSAS_G, AES_G, SDS_G, STA_G)
 
-fa.diagram(mixedCor(na.omit(cbind(SCZ_S, OCI_S, LSAS_S, AES_S, SDS_S, STA_S)),
-           method = 'spearman', d = 1:22, p = 23:122)$rho
-           )
+myCormatFull <- mixedCor(gillan_plotting_fa,
+                        method = 'spearman',
+                        d = 1:22, c = 23:122)
+myFaT <- fa(myCormatFull$rho,nfactors=16,n.obs=nrow(Gillan),rotate="oblimin",fm="gls")
+t1 <- data.frame(unclass(myFaT$loadings))
+# dplyr::select items that have at least one loading > 0.4
+ord <- colnames(gillan_plotting_fa)
+t3 <- data.table(t1, keep.rownames=TRUE)
+t3$rn <- factor(t3$rn,levels=ord)
+t2B <- melt.data.table(t3,id.vars="rn",variable.name="factors",value.name="loadings")
 
 
-mixedCor(complete_gillan[1:400,], method = 'spearman',
-             d = 1:22, p = 23:122)$rho
+# replace the labels
+plot.sixFactors <- ggplot(t2B,aes(rn,(loadings),fill=loadings)) +
+  facet_wrap(~ factors, nrow=1) +geom_bar(stat="identity",aes(fill=abs(loadings)>0.4))+
+  scale_fill_manual("legend",values=c("TRUE"="black","FALSE"="grey"))+coord_flip()+ylab("Loading strength")+xlab("") +theme_bw(base_size=10)+
+  theme(legend.position="none",axis.text.x=element_text(size=10),axis.text.y=element_text(size=10),text = element_text(size=10),strip.text = element_text(size = 10,face="bold"))+
+  ggtitle(" ")
+
+ggsave(plot.sixFactors, filename = "datasets/gillan_factors.png")
+
+
+myCormatFull <- mixedCor(scholl_plotting_fa,
+                        method = 'spearman',
+                        d = 1:22, c = 23:122)
+myFaT <- fa(myCormatFull$rho,nfactors=16,n.obs=nrow(Gillan),rotate="oblimin",fm="gls")
+t1 <- data.frame(unclass(myFaT$loadings))
+# dplyr::select items that have at least one loading > 0.4
+ord <- colnames(gillan_plotting_fa)
+t3 <- data.table(t1, keep.rownames=TRUE)
+t3$rn <- factor(t3$rn,levels=ord)
+t2B <- melt.data.table(t3,id.vars="rn",variable.name="factors",value.name="loadings")
+
+
+# replace the labels
+plot.sixFactors <- ggplot(t2B,aes(rn,(loadings),fill=loadings)) +
+  facet_wrap(~ factors, nrow=1) +geom_bar(stat="identity",aes(fill=abs(loadings)>0.4))+
+  scale_fill_manual("legend",values=c("TRUE"="black","FALSE"="grey"))+coord_flip()+ylab("Loading strength")+xlab("") +theme_bw(base_size=10)+
+  theme(legend.position="none",axis.text.x=element_text(size=10),axis.text.y=element_text(size=10),text = element_text(size=10),strip.text = element_text(size = 10,face="bold"))+
+  ggtitle(" ")
+
+ggsave(plot.sixFactors, filename = "datasets/gillan_factors.png")
+
+#############################################################
 
 complete_gillan <- cbind(SCZ_G, OCI_G, LSAS_G, AES_G, SDS_G, STA_G)
 
@@ -118,11 +161,23 @@ data_points <- seq(400, 1413, by = 20)
 bigout <- data.frame()
 for (i in data_points) {
     print(paste(which(data_points == i), '/', length(data_points), sep = ""))
+    current <- complete_gillan[sample(1:nrow(complete_gillan), i), ]
     rownames(current) <- 1:nrow(current)
     output <- fa.parallel(
-        mixedCor(na.omit(current), method = 'spearman', d = 1:22, p = 23:122, global = FALSE, use = 'na.or.complete.obs')$rho,
+        mixedCor(na.omit(current), method = 'spearman', d = 1:22, c = 23:122, global = FALSE, use = 'complete.obs')$rho,
         n.iter = 20, n.obs = i
         )
     print(output$nfact)
     bigout <- rbind(bigout, cbind(i, output$nfact, output$ncomp))
 }
+
+write.csv(bigout, 'datasets/FA_nfact.csv')
+
+bigout <- read.csv('datasets/FA_nfact.csv')
+
+ggplot(bigout, aes(x = i, y = V2)) + geom_point(size=3) + theme_bw()
+
+ggsave('FA_nfact.png')
+
+colnames(Gillan)
+
